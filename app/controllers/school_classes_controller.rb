082d1268e1595e9bb0a13cb6782c712a535606bd
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-05 23:08:26 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,6 +12,11 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = SchoolClass.new
+    @school_class.title = params[:title]
+    @school_class.room_number = params[:room_number]
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
