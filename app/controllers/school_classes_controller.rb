ef6f2007e3c8680e37354f10acef66763119e478
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-05 23:06:47 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,18 @@
 class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
