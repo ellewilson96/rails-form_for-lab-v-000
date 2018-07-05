7ed6a91163b4a3ba04ad42f43866ada27005c4a1
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-05 23:09:21 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -24,5 +24,12 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+
+    redirect_to student_path(@student)
   end
 end
